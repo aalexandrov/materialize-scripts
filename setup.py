d3ef46198bcb1dd8ab07566c1651c01eeea84768
Alexander Alexandrov <alexander.s.alexandrov@gmail.com>
2022-02-14 15:19:11 +0200

    use `psycopg2-binary` instead of `psycopg2`

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     install_requires=[
         "click==8.0.3",
         "lxml==4.7.1",
-        "psycopg2==2.9.1",
+        "psycopg2-binary==2.9.1",
         "types-pkg-resources==0.1.3",
     ],
     entry_points={
